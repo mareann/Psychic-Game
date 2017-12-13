995d39e1b19367579d23f74282c3d859d4f23c9c
Maryann Jordan <majch@DESKTOP-KQJEU0U.(none)>
2017-12-13 16:51:16 -0600

    added reset.css; verify user guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
     
-    // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+    // Creates an array that lists out all of the options.
     var computerChoices = ["a", "b", "c", "d", "e",
     "f", "g", "h", "i", "j", "k", "l", "m", "n", "o","p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var userGuesses = [];
@@ -12,7 +12,7 @@
        "<li id='displayWins'>Wins: " + w + "</li>"+
       "<li id='displayLoss'>Loss: " + l + "</li><li id='displayGuessCount'>Guesses Left: " + g + "</li>" +
       "<li id='displayGuess'>Your Guesses so far: " + userGuesses + "</li>";
-   }
+    }
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
 
@@ -20,27 +20,35 @@
     var userGuess = event.key;
 
     // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]; 
 
-      // check if already guessed that letter
-      var userGuessDuplicate = 0;
-      for ( var i = 0; i < userGuesses.length; i++)
+    // check if already guessed that letter
+    var userGuessDuplicate = 0;
+
+    // check if userGuess valid
+    var userGuessValid = 0;
+
+    for ( var i = 0; i < userGuesses.length; i++)
       {
         if ( userGuess === userGuesses[i])
-        {
-//          alert("you already guessed " + userGuess);
           userGuessDuplicate = 1;
-        }
       }
 
-      if ( userGuessDuplicate == 0)
+    for ( var i = 0; i < computerChoices.length; i++)
+    {
+      if ( userGuess === computerChoices[i])
+        userGuessValid = 1;
+    }
+
+    if ( ( userGuessDuplicate == 0) && ( userGuessValid ) )
       {
       //PUT userGuess into array userGuesses
       userGuesses.push(userGuess);
 
       console.log("you guessed "+ userGuess + " " + userGuesses);
       console.log("computer "+ computerGuess);
-      if (userGuess === computerGuess)
+
+    if (userGuess === computerGuess)
       {
       	// USER WINS
       	// increment userWins count
@@ -52,15 +60,15 @@
         screenDisplayUpdate(userWins,userLoss,guessesLeft);
         console.log("YOU WIN");
       }
-      else{
+    else{
       	guessesLeft--;
       	screenDisplayUpdate(userWins,userLoss,guessesLeft);
       }
 
-      // USER LOSS - NO MORE GUESSES
-      // increment userLoss count
-      // reset guessesLeft count to 9
-      if ( guessesLeft == 0 )
+    // USER LOSS - NO MORE GUESSES
+    // increment userLoss count
+    // reset guessesLeft count to 9
+    if ( guessesLeft == 0 )
       {
       	 userLoss++;
          guessesLeft = 9;
@@ -72,8 +80,11 @@
     else
     {
 
-      alert("Try again! You already guessed " + userGuess);
-
+        // not valid or duplicate userGuess
+        if ( userGuessValid )
+          alert("Try again! You already guessed " + userGuess);
+        else
+          alert("Try again! Not a letter " + userGuess);
     }
   }
   
